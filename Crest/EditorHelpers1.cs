16c2bc02e89bc0326f9eea7cc22e3f27a4c73daf
chenchai881010 <chen58839@gmail.com>
2022-10-18 21:17:15 +0800

    Create EditorHelpers1.cs

---
+++ Crest/EditorHelpers1.cs
@@ -0,0 +1,13 @@
+﻿// Crest Ocean System
+
+namespace Crest
+{
+    internal class EditorHelpers
+    {
+        public static object EditorHelpers { get; internal set; }
+
+        internal interface IEmbeddableEditor
+        {
+        }
+    }
+}
